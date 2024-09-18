1bb87fd234059c953bf7de47803730be88aacaa9
JuuhCarvalho16 <juliana.vivien@gmail.com>
2024-09-18 11:13:31 -0300

    Descrição das alterações feitas

---
+++ FRONT/src/js/login.js
@@ -0,0 +1,36 @@
+
+document.getElementById('registerForm')?.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value
+
+    
+        const response = await fetch('http://localhost:3000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name, email, password}),
+        });
+
+        const data = await response.json();
+        alert(data.message);
+    
+})
+
+document.getElementById('formRegister')?.addEventListener('submit', async function(e){
+    e.preventDefault();
+    const email = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+
+    const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password}),
+    }) 
+    const data = await response.json();
+    alert(data.message);
+})
