015216c9ad0e79c2b23c547d37a391472bcd4b30
JulianaCarvalho16 <juliana.vivien@gmail.com>
2024-10-07 11:12:38 -0300

    atualização

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,16 @@
+const btnEntrar = document.getElementById('entrar-js')
+const btnIncrever = document.getElementById('inscrever-js')
+
+const body = document.querySelector('body')
+
+btnEntrar.addEventListener('click', () => {
+    body.className = "entrar-js"
+})
+
+btnIncrever.addEventListener('click', () => {
+    body.className = "inscrever-js"
+})
+
 document.getElementById('registerForm')?.addEventListener('submit', async function(e) {
     e.preventDefault();
     const name = document.getElementById('name').value;
